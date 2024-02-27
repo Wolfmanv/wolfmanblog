0be32f66cd4bed2ef5bf5f93af22403f3a7424ca
Łukasz <1991luke@gmail.com>
2024-02-27 11:32:03 +0100

    multiple changes in multiple files

--- script.js
+++ script.js
@@ -27,14 +27,63 @@ menuItem.forEach(function (menuItem) {
 	menuItem.addEventListener('click', dropNavbar);
 });
 
-// const navShadow = () => {
-// 	if(window.scrollY >= 1000) {
-// 		nav.classList.add('navbar__color')
-// } else {
-// 	nav.classList.remove('navbar__color')
-// }
-// }
+
+
+
 
 bar.addEventListener('click', dropNavbar);
 button.addEventListener('click', rotateBurger);
-// window.addEventListener('scroll', navShadow)
+
+
+// Carousel js
+
+
+const container = document.querySelector('carousel__container')
+
+const images = document.querySelectorAll('.slide')
+
+
+
+
+
+images.forEach((slide, indx) => {
+
+slide.style.transform = `translateX(${indx * 100}%)`;
+
+
+});
+
+let curSlide = 0;
+
+let maxSlide = images.length - 1;
+
+
+const btn1 = document.querySelector('.next')
+
+
+btn1.addEventListener('click', function () {
+   if(curSlide === maxSlide) {
+    curSlide = 0;
+   } else {
+    curSlide++;
+   }
+
+images.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+});
+})
+
+
+const btn2 = document.querySelector('.previous')
+
+btn2.addEventListener('click', function () {
+    if (curSlide === 0) {
+        curSlide = maxSlide
+    } else {
+        curSlide--;
+    }
+
+    images.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    })
+})
